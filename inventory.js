be9f05eebb2f46b08f264dd85b0f539e43bf3168
rain-soaked-eli <ewiwiin@gmail.com>
2026-02-27 08:11:24 +0800

    Add files via upload

---
+++ inventory.js
@@ -0,0 +1,14 @@
+/* ADD NEW POSTS HERE. 
+   The script will build the "Entry Cards" for you.
+*/
+const inventory = [
+    // NEWEST POSTS GO TOP
+    {
+        file: "yap.html",
+        title: "test",
+        date: "2026-02-14",
+        desc: "test123"
+    }
+
+]
+
